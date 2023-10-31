46faa38c52503dfe815b34a20d70deca46a0b177
CodiProgs <samuil.codiWork@mail.ru>
2023-10-31 23:34:31 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    credentials: false,
+    credentials: true,
     allowedHeaders: [
       'Accept',
       'Authorization',
