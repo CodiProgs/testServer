48d0e59f63e333c6098122a873356fb788f3f0f8
CodiProgs <samuil.codiWork@mail.ru>
2023-10-31 23:27:55 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import * as CookieParser from 'cookie-parser';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    credentials: true,
+    credentials: false,
     allowedHeaders: [
       'Accept',
       'Authorization',
